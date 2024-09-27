5509f66c5efc5827f5194b1e9613875868f5e725
Michael Spears <mts18142027@gmail.com>
2024-09-27 17:01:46 -0400

    Added ExampleArch class

---
+++ arch/examplearch.hpp
@@ -0,0 +1,14 @@
+#include "arch.hpp"
+#include <string>
+using std::string;
+
+class ExampleArch : public Arch {
+	// Runs Before Linking
+	string initialize() {
+		return "example";
+	}
+	// Runs After Linking
+	string finalize() {
+		return "arch";
+	}
+}
